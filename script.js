0617c6633c7d606366034d28353cf7c14dd904d7
bishwas350 <crashmindboy10@gmail.com>
2025-06-24 11:16:10 +0600

    string "get and append"

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-const { log } = require("console")
-const url = require("url")
-log(url)
+// const { log } = require("console")
+// const url = require("url")
+// const browserUrl =`https://nodejs.com/search?q=job done now go to home`
+// log(url.parse(browserUrl))
+
+const { URL } = require("url");
+const myurl = new URL("https://example.org:8080/p/a/t/h?query=string#hash");
+console.log(myurl.searchParams.get("query"));
+myurl.searchParams.append("amm ","ami bari jabo")
+console.log(myurl)
