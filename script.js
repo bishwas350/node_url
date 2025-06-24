a4c4ca628aaaeab79dfbc19ee6b86a66f316cb37
bishwas350 <crashmindboy10@gmail.com>
2025-06-24 17:01:59 +0600

    complete

--- script.js
+++ script.js
@@ -11,5 +11,45 @@
 // myurl.searchParams.delete('age')
 // console.log(myurl.toString());
 
-
-
+const fs = require("fs")
+const http = require("http");
+const url = require("url");
+const event = require("events");
+const eventEmitter = new event();
+eventEmitter.on("fetchData",()=>{
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res)=>{
+       return res.json()
+    })
+    .then((bye)=>{
+        console.log(bye);
+        fs.writeFile("data.txt",JSON.stringify(bye), (err)=>{
+            if (err){
+                console.log(`error from write file ${err}`);
+                
+            }
+        })
+    })
+    
+})
+const server = http.createServer((req, res) => {
+  const parseUrl = url.parse(req.url);
+  console.log(parseUrl.pathname);
+  const { pathname } = parseUrl;
+  if (pathname == "/bye") {
+    eventEmitter.emit("fetchData")
+    fs.readFile("data.txt","utf-8",(err,bye)=>{
+        console.log(bye);
+        res.end(bye)
+    })
+  }
+  if (pathname == "/post") {
+    res.writeHead(200, {
+      "content-type": "text/plain",
+    });
+    res.end("done");
+  }
+});
+server.listen(4000, () => {
+  console.log(`http://localhost:${4000}`);
+});
