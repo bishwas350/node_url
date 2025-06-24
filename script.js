773ed21fe5f2b1af2064cf285065a428d94faf61
bishwas350 <crashmindboy10@gmail.com>
2025-06-24 15:33:49 +0600

    add and dlt

--- script.js
+++ script.js
@@ -3,8 +3,13 @@
 // const browserUrl =`https://nodejs.com/search?q=job done now go to home`
 // log(url.parse(browserUrl))
 
-const { URL } = require("url");
-const myurl = new URL("https://example.org:8080/p/a/t/h?query=string#hash");
-console.log(myurl.searchParams.get("query"));
-myurl.searchParams.append("amm ","ami bari jabo")
-console.log(myurl)
+// const { URL } = require("url");
+// const myurl = new URL("https://example.org:8080/p/a/t/h?query=string&age=20#hash");
+// console.log(myurl.searchParams.get("query"));
+// myurl.searchParams.append("amm ","ami bari jabo")
+// console.log(myurl)
+// myurl.searchParams.delete('age')
+// console.log(myurl.toString());
+
+
+
